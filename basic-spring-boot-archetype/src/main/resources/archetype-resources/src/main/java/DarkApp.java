1c63cfc9510887b0dc5889136cfe5e93a8e64f17
davidokun <davidokun99@gmail.com>
2017-11-02 20:53:05 -0500

    Java source files

---
+++ basic-spring-boot-archetype/src/main/resources/archetype-resources/src/main/java/DarkApp.java
@@ -0,0 +1,12 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkApp.class, args);
+	}
+}
---
+++ basic-spring-boot-archetype/src/main/resources/archetype-resources/src/main/java/service/DarkService.java
@@ -0,0 +1,12 @@
+package ${package}.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DarkService {
+
+
+    public String sayHelloDark() {
+        return "Hello Dark World!!!";
+    }
+}
